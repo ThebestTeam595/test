1fddc53e53c4f97e8744268fc1be25ec81c11b0d
ThebestTeam595 <thebestteam.git@gmail.com>
2024-09-16 21:58:09 -0500

    Update app.js

--- asset-v1/app.js
+++ asset-v1/app.js
@@ -4,5 +4,5 @@ var app=new function(){this.name="Into The Pit",this.version="v?",this.date="202
 {name:"9_mask",color:"802B5B",uniqsnd:!1},
 {name:"15_monty",color:"FFAA28",uniqsnd:!1},
 {name:"18_phoneguy",color:"A94915",uniqsnd:!1},
-{name:"21_puppet",color:"FFFFFF",uniqsnd:!1},
+{name:"21_puppet",color:"FFFFFF",uniqsnd:!0}
 ],this.bonusarray=[{name:"Eyes",src:"bonus-eyes-hd.mp4",code:"2,6,9,15,18",sound:"bonus-eyes",aspire:"aspire-eyes"},{name:"Eyes",src:"bonus-eyes-hd.mp4",code:"2,6,9,15,18",sound:"bonus-eyes",aspire:"aspire-eyes"},{name:"Eyes",src:"bonus-eyes-hd.mp4",code:"2,6,9,15,18",sound:"bonus-eyes",aspire:"aspire-eyes"},{name:"Eyes",src:"bonus-eyes-hd.mp4",code:"2,6,9,15,18",sound:"bonus-eyes",aspire:"aspire-eyes"},{name:"Eyes",src:"bonus-eyes-hd.mp4",code:"2,6,9,15,18",sound:"bonus-eyes",aspire:"aspire-eyes"}],this.unlockerarray=[];for(var n=0,o=this.animearray.length;n<o;n++){var a=this.animearray[n].name;this.animearray[n].soundA=a+"_a",this.animearray[n].soundB=this.animearray[n].uniqsnd?a+"_a":a+"_b",this.animearray[n].anime=a+"-sprite.png",this.animearray[n].animeData=a+".json"}};
